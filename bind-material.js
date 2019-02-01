523de422536759746d4f4a5897f015c7ad6255d1
Mark Hobson <mark.hobson@blackpepper.co.uk>
2019-02-01 16:09:38 +0000

    Remove Material adapter for now
    Requires too much markup and JS doesn't work without classes.

--- bind-material.js
+++
@@ -1,7 +0,0 @@
-[].map.call(document.querySelectorAll('.add-pet .name'), function(el) {
-	return new mdc.textField.MDCTextField(el);
-});
-
-[].map.call(document.querySelectorAll('.add-pet .animal'), function(el) {
-	return new mdc.select.MDCSelect(el);
-});
